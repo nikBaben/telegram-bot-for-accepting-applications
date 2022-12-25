6fc84624fd58518dfea78f8183ef6f3976c217f4
nikBaben <faymen44@mail.ru>
2022-12-25 21:26:35 +0300

    upgrade db

--- data.py
+++ data.py
@@ -8,4 +8,12 @@ async def create_db():
     cur.execute("CREATE TABLE IF NOT EXISTS GAMEHUB_DATA (user_id,user_name,phone_number,platform,console,problem)")
     db.commit()
 
+async def create_profile(user_id,user_name):
+    user = (user_id,user_name)
+    cur.execute("INSERT INTO GAMEHUB_DATA (user_id,user_name) VALUES (?,?)", user,)
+    db.commit()
 
+
+async def get_user(user_id):
+    a = cur.execute("SELECT user_name FROM GAMEHUB_DATA WHERE user_id = ?", (user_id,)).fetchone()
+    return a
